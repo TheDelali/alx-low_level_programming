52186147a0814073d16e2b6785c20fb77df08cea
TheDelali <ekuetsidzo3@gmail.com>
2023-03-21 19:55:48 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(letter);
 		for (j = 0; j < 10; j++)
-			_putchar('\n');
+			_putchar(letter);
+		_putchar('\n');
 	}
 }
