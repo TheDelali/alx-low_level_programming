5de5e2072bc573225a3d43bfde386902419f1ea4
TheDelali <ekuetsidzo3@gmail.com>
2023-03-22 02:28:04 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -8,17 +9,19 @@
 void jack_bauer(void)
 {
 	int hour, minute;
+	char buffer[6];
 
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; hour < 60; minute++)
 		{
-			_putchar(hour / 10);
-			_putchar(hour % 10);
-			_putchar(':');
-			_putchar(minute / 10);
-			_putchar(minute % 10);
-			_putchar('\n');
+			buffer[0] = hour / 10 + '0';
+			buffer[1] = hour % 10 +'0';
+			buffer[2] = ':';
+			buffer[3] = minute / 10 +'0';
+			buffer[4] = minute % 10 +'0';
+			buffer[5] = '\n';
+			write(STDOUT_FILENO, buffer, 6);
 		}
 	}
 }
