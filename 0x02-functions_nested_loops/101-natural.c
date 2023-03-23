757fd9651e27fa3ff92317c036ce9b8514be51b2
TheDelali <ekuetsidzo3@gmail.com>
2023-03-23 03:14:34 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,7 @@
-/**
- * File: 101-natural.c
- * Auth: TheDelali
-*/
-
 #include <stdio.h>
 
 /**
- * main -lists all the natural numbers below 1024(excluded)
+ * main -lists all the natural numbers below 1024(excluded) 
  * that are multiples of 3 or 5
  * Return: 0
 */
