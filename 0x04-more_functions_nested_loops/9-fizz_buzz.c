ac55d373622658685683fa98c1cc35170cecfc02
TheDelali <ekuetsidzo3@gmail.com>
2023-03-24 14:33:10 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 	int i;
+	i
 	for (i = 1; i <= 100; i++)
 	{
-		printf("%i ", i);
-
 		if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else if (i % 15 == 0)
 			printf("FizzBuzz ");
+		else 
+			printf("%i ", i);
 	}
 	printf("\n");
 	return (0);
