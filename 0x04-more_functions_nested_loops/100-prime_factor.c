1e199d6f8749eae407554f53827796c9cb373ef6
TheDelali <ekuetsidzo3@gmail.com>
2023-03-25 14:43:28 +0000

    printing largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -prints the largest prime factor of 612852475143
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	long long n = 612852475143;
+	long long i;
+
+	for (i = 2; i <= n; i++)
+	{
+		while (n % i == )
+		{
+			n = n / i;
+		}
+	}
+	printf("%lld\n", i-1);
+
+	return (0);
+}
