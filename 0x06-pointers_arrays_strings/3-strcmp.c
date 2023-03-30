098cee595a2cc0074c1bffe22ee621440aa13ffd
TheDelali <ekuetsidzo3@gmail.com>
2023-03-30 05:35:53 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare 2 string
+ * @s1: string to compare
+ * @s2: string to compare
+ * Return: 0 if same
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
