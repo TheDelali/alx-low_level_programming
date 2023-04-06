8383b6520c78fbad06a01c1277ae61d706a399f1
TheDelali <ekuetsidzo3@gmail.com>
2023-04-06 10:23:48 +0000

    compute prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * is_prime_number -checks if input integer is prime
+ * @n: the integer to check
+ *
+ * Return: 1 if integer is prime and 0 if otherwise
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	int i;
+
+	for (i = 2; i <= n/2; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
