458d9e3b9a3cd2407374f890b67244b1a1414344
TheDelali <ekuetsidzo3@gmail.com>
2023-04-14 02:44:31 +0000

    create array of imtegers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,1 +1,25 @@
 #include "main.h"
+
+/**
+ * array_range -creates an array of integers
+ * @min: startpoint
+ * @max: endpoint
+ *
+ * Return: an array or NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int len, i;
+	int *p;
+
+	if (min > max)
+		return (NULL);
+	len = max - min + 1;
+	p = malloc(sizeof(int) * len);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		p[i] = min++;
+	return (p);
+}
