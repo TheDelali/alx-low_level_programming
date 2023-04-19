d7a2339fd5e5f01a66415316a991aa2c7dd59712
TheDelali <ekuetsidz3@gmail.com>
2023-04-19 09:16:53 +0000

    concatenate arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * argstostr -concatenates arguments of program
+ * @ac: argument counter
+ * @av: argument vector
+ *
+ * Return: pointer to new string or NULL if failed
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int totallen = 0;
+	int i;
+	char *result;
+	int pos = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		totallen += strlen(av[i]) + 1;
+
+	result = (char*) malloc(sizeof(char) * totallen);
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(result + pos, av[i]);
+		pos += strlen(av[i]);
+		result[pos++] = '\n';
+	}
+
+	return (result);
+}
