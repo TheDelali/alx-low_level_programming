bf3012533b3165cff75696501dfea34e2455ab51
TheDelali <ekuetsidz3@gmail.com>
2023-04-20 17:46:51 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -new type to describe dog
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
