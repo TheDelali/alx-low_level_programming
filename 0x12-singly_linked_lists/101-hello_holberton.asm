74ffe218ccde8b9d55658ce0378984b2338e9399
TheDelali <ekuetsidz3@gmail.com>
2023-04-28 04:37:18 +0000

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
 section .data
-	msg db "Hello, Holberton", 0Ah
+	msg db "Hello, Holberton", 0Ah ; msg to print with newline char
 
 section .text
 	global main
 
-	extern printf
+	extern printf ; declare printf function
 
 main:
-	mov rdi, msg
-	xor eax, eax
-	call printf
-	ret
+	mov rdi, msg ; move msg address to first arg register
+	xor eax, eax ; clear eax register for success
+	call printf ; call printf function
+	ret ; return from main
