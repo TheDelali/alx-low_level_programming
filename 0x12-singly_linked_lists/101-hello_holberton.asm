63455d43013c68b7f590c8a8f801ba7761336a3e
TheDelali <ekuetsidz3@gmail.com>
2023-04-28 04:59:41 +0000

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,36 +1,13 @@
 section .data
-    ; Define a null-terminated string "Hello, Holberton" followed by a line feed
-    str: db "Hello, Holberton", 0xa
-    
-    ; Calculate the length of the string
-    len equ $ - str
-	
-section .text
+	msg db "Hello Holberton", 0Ah
 
-; Define the entry point of the program
-global main
+section .text
+	global main
+
+	extern printf
 
 main:
-  ; Load the system call number for write into RAX
-  MOV RAX, 1
-
-  ; Load the file descriptor for standard output into RDI
-  MOV RDI, 1
-
-  ; Load the address of the message into RSI
-  MOV RSI, str
-
-  ; Load the length of the message into RDX
-  MOV RDX, len
-
-  ; Make the system call using the syscall instruction
-  syscall
-
-  ; Load the system call number for exit into RAX
-  MOV RAX, 60
-
-  ; Load the exit status into RDI
-  MOV RDI, 0
-
-  ; Make the system call using the syscall instruction
-  syscall
+	mov rdi, msg
+	xor eax, eax
+	call printf
+	ret
